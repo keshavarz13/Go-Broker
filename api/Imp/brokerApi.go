0520ebc34186be7fe086ca39867055c1db279a4f
m.keshavarz <keshavarz.ma13@gmail.com>
2023-03-13 21:59:27 +0330

    Move grpc api from main.go to another file

---
+++ api/Imp/brokerApi.go
@@ -0,0 +1,57 @@
+package apiImp
+
+import (
+	"context"
+	"log"
+	"net"
+	"therealbroker/api/proto"
+	internalBroker "therealbroker/internal/broker"
+	"therealbroker/pkg/broker"
+	"time"
+
+	"google.golang.org/grpc"
+)
+
+type server struct {
+	proto.UnimplementedBrokerServer
+	broker broker.Broker
+}
+
+func (s *server) Publish(ctx context.Context, in *proto.PublishRequest) (*proto.PublishResponse, error) {
+	result, err := s.broker.Publish(ctx, in.Subject, broker.Message{
+		Expiration: time.Second * time.Duration(in.ExpirationSeconds), Body: string(in.Body),
+	})
+	log.Println(err)
+	return &proto.PublishResponse{Id: int32(result)}, err
+}
+
+func (s *server) Subscribe(in *proto.SubscribeRequest, srv proto.Broker_SubscribeServer) error {
+	ch, _ := s.broker.Subscribe(nil, in.Subject)
+	for {
+		select {
+		case msg := <-ch:
+			srv.Send(&proto.MessageResponse{Body: []byte(msg.Body)})
+		}
+	}
+	return nil
+}
+
+func (s *server) Fetch(ctx context.Context, in *proto.FetchRequest) (*proto.MessageResponse, error) {
+	result, err := s.broker.Fetch(ctx, in.Subject, int(in.Id))
+	log.Println(err)
+	return &proto.MessageResponse{Body: []byte(result.Body)}, err
+}
+
+func RunGrpcServer() {
+	listener, err := net.Listen("tcp", ":5100")
+	if err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+
+	s := grpc.NewServer()
+	myServer := server{broker: internalBroker.NewModule()}
+	proto.RegisterBrokerServer(s, &myServer)
+	if err := s.Serve(listener); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module therealbroker
 go 1.15
 
 require (
-	github.com/stretchr/testify v1.7.0
+	github.com/gorilla/mux v1.8.0
+	github.com/prometheus/client_golang v1.12.1
+	github.com/stretchr/testify v1.7.1
 	google.golang.org/grpc v1.46.0
 	google.golang.org/protobuf v1.28.0
 )
